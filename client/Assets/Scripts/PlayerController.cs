4c01273be54b70c20374c3740c02f5f9dd3f5b37
a3ry <doau@oregonstate.edu>
2025-11-09 22:51:20 -0800

    Update PlayerController.cs
    Update PlayerController for FPS movement

--- client/Assets/Scripts/PlayerController.cs
+++ client/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotateSpeed = 100f;
-
+    
     void Update()
     {
         // Get input from keyboard
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // Move forward/backward
-        transform.Translate(Vector3.forward * vertical * moveSpeed * Time.deltaTime);
-
-        // Rotate left/right
-        transform.Rotate(Vector3.up * horizontal * rotateSpeed * Time.deltaTime);
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical");     // W/S or Up/Down
+        
+        // Move in direction player is facing
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal;
+        transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
-
