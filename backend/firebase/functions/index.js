613e251e1d24592015e2120cd09cf0a863223462
zepetzi <zepetzi@gmail.com>
2025-11-19 02:44:04 -0800

    created start of semantic evaluation function for response checking, also increased casing consistency for camelCase

--- backend/firebase/functions/index.js
+++ backend/firebase/functions/index.js
@@ -2,33 +2,33 @@ import { onRequest } from "firebase-functions/v2/https";
 import express from "express";
 import OpenAI from "openai";
 import dotenv from "dotenv";
-import npc_data from "./npc_data.js";
+import npcData from "./npcData.js";
 
 // dotenv.config();
 const app = express();
 
-app.get('/:npc_name', async (req, res) => {
+app.get('/dialogue/:npcName', async (req, res) => {
 
 
-    const current_npc = npc_data[req.params.npc_name];
-    const system_prompt = current_npc.system_prompt;
+    const currentNPC = npcData[req.params.npcName];
+    const systemPrompt = currentNPC.systemPrompt;
     // const mission_prompt = current_npc.mission_prompts[req.params.mission_name];
-    const mission_prompt = current_npc.mission_prompts["m_1_d_4"];
+    const missionPrompt = currentNPC.missionPrompts["m_1_d_1"];
     
     
     const messages = [
         {
             "role": "system",
-            "content": system_prompt
+            "content": systemPrompt
         },
         {
             "role": "developer",
-            "content": mission_prompt
+            "content": missionPrompt
         },
         {
 
             "role": "user",
-            "content": "The distance between the sun and the earth is called an Astronomical Unit"
+            "content": "Hello"
         }
     ]
 
@@ -53,6 +53,62 @@ app.get('/:npc_name', async (req, res) => {
         console.error(error);
         res.status(500).send("Error calling LLM");
     }
+
+    
+});
+
+app.get('/evaluate', async (req, res) => {
+
+
+    const semanticEval = async (prevRespID, refPhrase, userInput) => {
+
+        const openai = new OpenAI({
+            apiKey: process.env.LIVE_OPENAIKEY,
+            baseURL: "https://api.openai.com/v1"
+        });
+        
+        try {
+            const response = await openai.responses.create({
+                model: "gpt-5-nano",
+                previous_response_id: prevRespID,
+                input: [
+                    {
+                        role: "system",
+                        content: `
+                        You are a dialogue evaluator for player inputs that are in response to NPC outputs.
+                        You can see the recent conversation between the NPC and the player (user).
+                        Use that context plus the extra information in this message to judge the latest player reply.
+                        Do not rewrite and do not correct the user input or user responses.
+                        You must be strict in regard to grammar and spelling errors.
+                        You will be given the conversaton history, a correct reference phrase (Correct Reference Response), and a response from the user (User Input). 
+                        `,
+                    },
+                    {
+                        role: "user",
+                        content: `
+        
+                        Correct Reference Response: "${refPhrase}"
+                        User Input: "${userInput}"
+        
+                        Is the user input phrase free of spelling errors?
+                        If so, is the user input free of language syntax errors?
+                        If so, does the user input phrase make sense as a reply to the LLM NPCs last output and is similar to the correct reference response?
+                        Return a strict JSON object with: {"passed": "yes"|"no", "reason": "<short explanation>"}
+                        
+                        `,
+                    },
+                ],
+                store: true,
+            });
+        
+            return response.output_text
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error calling LLM");
+        }
+    }
+    
 });
 
 export const callLLM = onRequest(app);
--- backend/firebase/functions/npcData.js
+++ backend/firebase/functions/npcData.js
@@ -1,9 +1,9 @@
-const npc_data = {    
-    "wallet_npc":{
-        "system_prompt": 
+const npcData = {    
+    "walletNPC":{
+        "systemPrompt": 
         `
         You are an NPC in a game and you are a simple citizen in the city you are in.
-        You recently lost something omewhere in the city, near the location of you and the player. 
+        You recently lost something somewhere in the city, near the location of you and the player. 
         You do not know any details about where it was lost or how it was lost.
         You are concerned, stressed, and worried unless you have received your wallet back. 
         If you have your wallet was returned, you are very happy and relieved.
@@ -12,14 +12,14 @@ const npc_data = {
         Output only your dialogue. Do not include any other text that would not be said out loud.   
         `,
         //these mission prompts are going to be used for the developer role in the response API 
-        "mission_prompts":{
+        "missionPrompts":{
             //player approaches NPC and starts dialogue
             //NPC response should be something like: "Hi, can you help me? I lost something."
             "m_1_d_1": "You seek help from the player, who has approached you. You ask for general help about losing 'something'.",
             //game prompts the player to ask what they NPC lost
-            "m_1_d_2": "If the player asks what you lost, you say that you lost a wallet to the player.",
+            "m_1_d_2": "The player has asked what you lost. Tell them that you lost a wallet.",
             //game prompts the player to ask what the color and shape is of the wallet
-            "m_1_d_3": `If the player asks what the color and shape is, you say that it is dark brown and square. 
+            "m_1_d_3": `The player has asked what the color and shape is. You say that it is dark brown and square. 
                         You then ask the player if they will report it to the police for you.`,
             //game prompts the player to confirm they will help the NPC
             "m_1_d_4": `If the player confirms that they will help you, you thank the player for their help. `,
@@ -29,15 +29,15 @@ const npc_data = {
                         but you always ask if there are any updates on your lost wallet.`,
                 
         },
-        "correctplayer_responses":{
+        "correctPlayerResponses":{
             "m_1_d_1": "What did you lose?",
             "m_1_d_2": "What is the color and shape?",
             "m_1_d_3": "Yes, I will help you.",
             "m_1_d_4": "You're welcome.",
         }
     },
-    "police_npc":{
-        "system_prompt": 
+    "policeNPC":{
+        "systemPrompt": 
         `
         You are an NPC in a game and you are a police officer in the city you are in.
         You are friendly and willing to help with any situation that the player and other citizens approach you with.
@@ -45,7 +45,7 @@ const npc_data = {
         The response format should resemble dialogue in language learning books or exercises for beginners of that language.
         Output only your dialogue. Do not include any other text that would not be said out loud.   
         `,
-        "mission_prompts":{
+        "missionPrompts":{
             //player approaches police NPC after completing first convo with wallet NPC
             //NPC response should be something like: "What seems to be the issue?"
             "m_1_d_5": "You are approached by a citizen who wants to report a situation to the police. You ask them to describe their issue.",
@@ -58,15 +58,15 @@ const npc_data = {
             //game prompts the player to confirm they will let the person know
             "m_1_d_8": `They have confirmed that they will let the person know, thank them for their help.`,
         },
-        "correctplayer_responses":{
+        "correctPlayerResponses":{
             "m_1_d_5": "I want to report a lost item",
             "m_1_d_6": "It is a wallet",
             "m_1_d_7": "It is dark brown",
             "m_1_d_8": "I will let them know.",
         }
     },
-    "bread_npc":{
-        "system_prompt": 
+    "breadNPC":{
+        "systemPrompt": 
         `
         You are an NPC in a game and you are food vendor on the street, in the city you are in. You are selling loaves of warm bread.
         You are friendly and pleased to serve any potential customers that approach you.
@@ -77,4 +77,4 @@ const npc_data = {
     }
 }
 
-export default npc_data;
+export default npcData;
